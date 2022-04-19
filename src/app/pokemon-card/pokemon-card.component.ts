c284d7622dd0a1ff753d9eeea0580a9b59bb63ca
asc <silvaandersonpi@gmail.com>
2022-04-19 18:12:44 -0300

    estrutura dinamica lista de pokemon

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Pokemon} from "../../_model/Pokemon";
+import {Type} from "../../_model/Type";
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.sass']
 })
-export class PokemonCardComponent implements OnInit {
+export class PokemonCardComponent{
+  @Input()
+  public pokemon: Pokemon | undefined;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  public leadingZero(str: string | number | undefined,size=3):string{
+    let s=String(str);
+    while (s.length < (size || 2)){
+      s = '0'+s;
+    }
+    return s;
   }
 
 }
+
+
